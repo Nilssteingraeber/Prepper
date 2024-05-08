f2ca47fd079cd0597ab822c0f90cfb01801d7046
Nils Steingräber <nils.steingraeber@iqbis.de>
2024-05-08 20:32:02 +0200

    Bisschen getested

--- prepperApp/src/App.vue
+++ prepperApp/src/App.vue
@@ -1,85 +1,53 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import LoginButton from './components/homePage/LoginButton.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="appBody">
+    <div class="header">
+      <img
+        @click="count++"
+        alt="Prepper logo"
+        class="logo"
+        src="@/assets/logo_prepper.svg"
+        width="125"
+        height="125"
+      />
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
     </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.header {
+  
+}
+.appBody {
+  margin: 1rem;
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  top: 0px;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  margin: 2rem 2rem 0 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  padding: 1rem 0;
+  margin-top: 1rem;
 }
 </style>
---
+++ prepperApp/src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <div class="frame">
+      <div class="loginTitle">
+        This is the login page
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .loginTitle {
+    display: inline;
+    font-size: xx-large;
+  }
+
+  .login {
+    min-height: 100vh;
+    margin: 2%;
+    padding: 2%;
+    align-items: center;
+
+  .frame {
+    margin-left: 30%;
+    margin-right: 30%;
+    display: flex;
+    align-items: center;
+    border: 2%;
+    border-style: solid;
+    border-radius: 16px;
+    padding: 5%;
+    border-color: black;
+    
+  }
+}
+</style>
